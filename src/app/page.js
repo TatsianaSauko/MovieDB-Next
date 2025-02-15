b96bb4fc57f268f1edb39aab3cc2a26e6db22208
TatsianaSauko <miljanzowa@mail.ru>
2025-02-15 18:39:22 +0300

    feat: fetch data

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,34 @@
-import React from 'react'
+const API_KEY = process.env.API_KEY
+import Results from '@/components/Results';
 
-export default function Home() {
+
+export default async function Home({searchParams}) {
+  // const genre = searchParams?.genre || 'fetchTrending';
+    let results;
+
+    try {
+        const res = await fetch(
+            `https://cinemaguide.skillbox.cc/movie`,
+            {
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            }
+        );
+
+        if (!res.ok) {
+            throw new Error('Failed to fetch data');
+        }
+
+        results = await res.json();
+        console.log(results)
+    } catch (error) {
+        console.error('Error fetching data:', error);
+        data = { error: 'Failed to fetch data' };
+    }
   return (
-    <div>Home</div>
+    <div>
+    <Results results={results} />
+  </div>
   )
 }
