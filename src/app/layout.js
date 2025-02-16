24b3e17b3b30c0453d9a727a14d54f533417de0f
TatsianaSauko <miljanzowa@mail.ru>
2025-02-16 23:19:57 +0300

    refactor: move Provider

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
-import Providers from "./Providers";
+import Providers from "@/components/Providers";
 import Navbar from "@/components/Navbar";
 import SearchBox from "@/components/SearchBox";
 
@@ -23,9 +23,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body>
         <Providers>
           <Header />
           <Navbar />
